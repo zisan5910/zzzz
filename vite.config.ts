c14af0bcd6d949bd35c9fe79070b80fe478bdc8d
zisan5910 <zisan5910@gmail.com>
2025-10-03 03:45:41 +0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,12 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  publicDir: "public",
 }));
